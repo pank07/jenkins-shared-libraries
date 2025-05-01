31cb93302489faed92a76b3dda87df82f25494a4
pank07 <159031153+pank07@users.noreply.github.com>
2025-05-01 15:46:18 +0530

    docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,7 +1,7 @@
 def call(String Project, String ImageTag, String dockerhubuser){
   withCredentials([usernamePassword(crendentialsId:'dockerHubCred',
   passwordVariable:'dockerHubPass', usernameVaribale: 'dockerHubuser')])
-  sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
-}
-sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
+  {
+  sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+  sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
 }
